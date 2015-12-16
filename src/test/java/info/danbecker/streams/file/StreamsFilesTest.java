1dee2381f6a0f33dcee5b621cf0ac44ff29b9e2e
Dan Becker <dabecker@paypal.com>
2015-12-16 14:18:07 -0600

    Working map, filter, collect

--- src/test/java/info/danbecker/streams/file/StreamsFilesTest.java
+++ src/test/java/info/danbecker/streams/file/StreamsFilesTest.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -24,18 +25,18 @@ public class StreamsFilesTest {
 	@Test
 	public void testRegExStream() {		
 		// Read file into stream, try-with-resources
-		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+		try (Stream<String> lines = Files.lines(Paths.get(fileName))) {
 			Map<String,String> map = new Hashtable<String,String>();
 			
-			stream.forEach(text -> {
+			lines.forEach(text -> {
 				Matcher m = regEx.matcher(text);
-				if (m.find()) {
+				if (m.matches()) {
 					map.put(m.group("key"), m.group("value"));
-					// System.out.println( "key=" + m.group( "key" ) + ",value="
-					// + m.group( "value" ));
+					// System.out.println( "key=" + m.group( "key" ) + ",value=" + m.group( "value" ));
 				}
 			});
 			Assert.assertEquals( "Collection size", 10, map.size());
+			Assert.assertEquals( "Collection element", "valueJ", map.get( "key9" ));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}		        
@@ -44,15 +45,18 @@ public class StreamsFilesTest {
 	@Test
 	public void testRegExFilter() {		
 		// Read file into stream, try-with-resources
-//		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-//			Map<String,String> map = new Hashtable<String,String>();
-//			stream
-//				.forEach(text -> regEx.matcher(text))
-//				.filter( matcher -> matcher.find())
-//				.reduce( map -> map.put(m.group("key"), m.group("value")));
-//			Assert.assertEquals( "Collection size", 10, map.size());
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}		        
+		try (Stream<String> lines = Files.lines(Paths.get(fileName))) {
+			Map<String,String> map = lines
+			    .map(text -> regEx.matcher(text))
+				.filter(matcher -> matcher.matches())
+				// .forEach(matcher -> System.out.format("key=%s, value=%s\n", matcher.group("key"), matcher.group("value") ));
+				.collect( Collectors.toMap( matcher -> matcher.group("key"), matcher -> matcher.group("value")))
+			;
+			// System.out.println( "Map=" + map);
+			Assert.assertEquals( "Collection size", 10, map.size());
+			Assert.assertEquals( "Collection element", "valueJ", map.get( "key9" ));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		        
 	}
 }
